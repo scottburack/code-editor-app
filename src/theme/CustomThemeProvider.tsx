3e092a63bd1d327d28c0a9c99e86d9fc92eb6537
Scott Burack <itsmescottb123@gmail.com>
2021-11-18 17:36:00 -0500

    setup theme provider

---
+++ src/theme/CustomThemeProvider.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { commonColors, lightColors, darkColors } from './colors';
+import { light } from '@mui/material/styles/createPalette';
+
+const CustomThemeProvider = (props: PropsWithChildren<{}>) => {
+
+  const darkMode = false;
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      primary: {
+        main: commonColors.primary
+      }
+    },
+    background: darkMode ? darkColors.background : lightColors.background,
+    font: darkMode ? darkColors.font : lightColors.font,
+    commonColors,
+  })
+
+  return (
+    <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+  )
+}
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    background: string;
+    font: string;
+    commonColors: {[key: string]: string};
+  }
+
+  interface ThemeOptions {
+    background: string;
+    font: string;
+    commonColors: {[key: string]: string};
+  }
+}
+
+export default CustomThemeProvider
