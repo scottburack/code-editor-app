740d7af5692e8a4e39af380e4b0957f58ef5af78
Scott Burack <itsmescottb123@gmail.com>
2021-12-31 11:46:58 -0500

    add redux-persist

--- src/store/StoreProvider.tsx
+++ src/store/StoreProvider.tsx
@@ -1,10 +1,17 @@
 import { PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
-import store from '.';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from '../components/common/loading/Loading';
+import store, { persistor } from '.';
+
 
 const StoreProvider = (props: PropsWithChildren<{}>) => {
   return (
-    <Provider store={store}>{ props.children }</Provider>
+    <Provider store={store}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        { props.children }
+      </PersistGate>
+    </Provider>
   )
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
-import combinedReducers from "./root-reducer";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistStore } from 'redux-persist';
+import persistedReducer from "./root-reducer";
 
 const store = configureStore({
-  reducer: combinedReducers,
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE]
+    }
+  })
 })
+
+export const persistor = persistStore(store);
 
 export default store;
